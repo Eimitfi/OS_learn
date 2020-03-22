258b5e791cd36ff50cc1a708da12f7e31e7a9fa2
Eimitfi <macarena7799@gmail.com>
2020-03-22 01:48:50 +0100

    recursive task list

--- ker_task/k_tas.c
+++ ker_task/k_tas.c
@@ -6,11 +6,24 @@
 struct task_struct *task;
 struct list_head *list;
 
+static void rec_tree(struct task_struct *loc_task){
+	struct list_head *loc_list;
+	struct task_struct *tmp;
+	if(loc_task==NULL)
+		return;
+	list_for_each(loc_list,&((loc_task)->children)){
+		tmp=list_entry(loc_list,struct task_struct,sibling);
+		printk(KERN_INFO "%s   %ld   %d\n",tmp->comm,tmp->state,tmp->pid);
+		rec_tree(tmp);
+	}
+}
+
 static int init_mod(void){
 	printk(KERN_INFO "process name:    state:      id:");
 	list_for_each(list,&((&init_task)->children)){
 		task=list_entry(list,struct task_struct,sibling);
 		printk(KERN_INFO "%s   %ld   %d\n",task->comm,task->state,task->pid);
+		rec_tree(task);
 	}
 	return 0;
 }
